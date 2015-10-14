0d9873ba059bcacc45c6e6dce348cb784753141b
Michael Entrup <entrup@arcor.de>
2015-10-14 14:20:58 +0200

    Formatierung von Multithreading_Example.js korrigieren.

--- js/Multithreading_Example.js
+++ js/Multithreading_Example.js
@@ -35,65 +35,65 @@ importClass(Packages.java.lang.Thread);
 importClass(Packages.java.util.concurrent.atomic.AtomicInteger);
 // Print all numbers from start to end (inclusive), multithreaded
 function doItMultithreaded(start, end) {
-var threads = new java.lang.reflect.Array.newInstance(java.lang.Thread, Runtime.getRuntime().availableProcessors());
-var ai = new AtomicInteger(start);
-var body = {
-run: function() {
-for (var i = ai.getAndIncrement(); i <= end; i = ai.getAndIncrement()) {
-IJ.log("i is " + i);
-Thread.sleep(100); // NOT NEEDED, just to pretend we are doing something!
-}
-}
-}
-// start all threads
-for (var i = 0; i < threads.length; i++) {
-threads[i] = new Thread(new Runnable(body)); // automatically as Runnable
-threads[i].start();
-}
-// wait until all threads finish
-for (var i = 0; i < threads.length; i++) {
-threads[i].join();
-}
+	var threads = new java.lang.reflect.Array.newInstance(java.lang.Thread, Runtime.getRuntime().availableProcessors());
+	var ai = new AtomicInteger(start);
+	var body = {
+		run: function() {
+			for (var i = ai.getAndIncrement(); i <= end; i = ai.getAndIncrement()) {
+				IJ.log("i is " + i);
+				Thread.sleep(100); // NOT NEEDED, just to pretend we are doing something!
+			}
+		}
+	}
+	// start all threads
+	for (var i = 0; i < threads.length; i++) {
+		threads[i] = new Thread(new Runnable(body)); // automatically as Runnable
+		threads[i].start();
+	}
+	// wait until all threads finish
+	for (var i = 0; i < threads.length; i++) {
+		threads[i].join();
+	}
 }
 // execute like:
 // doItMultithreaded(0, 10);
 // Now, abstract away the multithreading framework into a function
 // that takes another function as argument:
 function multithreader(fun, start, end) {
-var threads = new java.lang.reflect.Array.newInstance(java.lang.Thread, Runtime.getRuntime().availableProcessors());
-var ai = new AtomicInteger(start);
-// Prepare arguments: all other arguments passed to this function
-// beyond the mandatory arguments fun, start and end:
-var args = new Array();
-var b = 0;
-IJ.log("Multithreading function \"" + fun.name + "\" with arguments:\n argument 0 is index from " + start + " to " + end);
-for (var a = 3; a < arguments.length; a++) {
-args[b] = arguments[a];
-IJ.log(" argument " + (b+1) + " is " + args[b]);
-b++;
-}
-var body = {
-run: function() {
-for (var i = ai.getAndIncrement(); i <= end; i = ai.getAndIncrement()) {
-// Execute the function given as argument,
-// passing to it all optional arguments:
-fun(i, args);
-}
-}
-}
-// start all threads
-for (var i = 0; i < threads.length; i++) {
-threads[i] = new Thread(new Runnable(body)); // automatically as Runnable
-threads[i].start();
-}
-// wait until all threads finish
-for (var i = 0; i < threads.length; i++) {
-threads[i].join();
-}
+	var threads = new java.lang.reflect.Array.newInstance(java.lang.Thread, Runtime.getRuntime().availableProcessors());
+	var ai = new AtomicInteger(start);
+	// Prepare arguments: all other arguments passed to this function
+	// beyond the mandatory arguments fun, start and end:
+	var args = new Array();
+	var b = 0;
+	IJ.log("Multithreading function \"" + fun.name + "\" with arguments:\n argument 0 is index from " + start + " to " + end);
+	for (var a = 3; a < arguments.length; a++) {
+		args[b] = arguments[a];
+		IJ.log(" argument " + (b+1) + " is " + args[b]);
+		b++;
+	}
+	var body = {
+		run: function() {
+			for (var i = ai.getAndIncrement(); i <= end; i = ai.getAndIncrement()) {
+				// Execute the function given as argument,
+				// passing to it all optional arguments:
+				fun(i, args);
+			}
+		}
+	}
+	// start all threads
+	for (var i = 0; i < threads.length; i++) {
+		threads[i] = new Thread(new Runnable(body)); // automatically as Runnable
+		threads[i].start();
+	}
+	// wait until all threads finish
+	for (var i = 0; i < threads.length; i++) {
+	threa	ds[i].join();
+	}
 }
 // The actual desired effect: the printer
 function printer(i) {
-IJ.log("i is " + i);
+I	J.log("i is " + i);
 }
 // Execute like (uncomment!):
 // multithreader(printer, 0, 10);
@@ -111,22 +111,22 @@ IJ.log("i is " + i);
 // Takes a starting line and a number of lines to process,
 // and sets their pixels to a random value
 function randomizer(line, args) {
-// Obtain and check the arguments:
-if (args.length < 5) {
-IJ.log("randomizer needs at least 5 arguments: line, pix, width, height, n_lines and rand");
-return;
-}
-var pix = args[0];
-var width = args[1];
-var height = args[2];
-var n_lines = args[3];
-var rand = args[4];
-for (var y = line; y < height && y < height + n_lines; y++) {
-var offset = y * width;
-for (var x = 0; x < width; x++) {
-pix[offset + x] = rand.nextFloat();
-}
-}
+	// Obtain and check the arguments:
+	if (args.length < 5) {
+		IJ.log("randomizer needs at least 5 arguments: line, pix, width, height, n_lines and rand");
+		return;
+	}
+	var pix = args[0];
+	var width = args[1];
+	var height = args[2];
+	var n_lines = args[3];
+	var rand = args[4];
+	for (var y = line; y < height && y < height + n_lines; y++) {
+		var offset = y * width;
+		for (var x = 0; x < width; x++) {
+			pix[offset + x] = rand.nextFloat();
+		}
+	}
 }
 // Test: create a new image, fill it with random values, and show it
 width = 512;
@@ -140,8 +140,7 @@ n_blocks = ((height / block_size)|0) + 1; // casting to int with bitwise or to z
 // Execute the randomizer in multithreaded fashion:
 // - At the top row, the three arguments for the multithreading framework
 // - At the bottom row, the N arguments for the function to parallelize
-multithreader(randomizer, 0, n_blocks,
-pix, width, height, block_size, rand);
+multithreader(randomizer, 0, n_blocks, pix, width, height, block_size, rand);
 // Show the image:
 imp.getProcessor().setMinAndMax(0, 1); // random values between 0 and 1
 imp.show();
