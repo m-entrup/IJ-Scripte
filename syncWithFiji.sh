1037b12254f794459f62ebc4e489e6add73874e6
Michael Entrup (b. Epping) <mail@m-entrup.de>
2015-11-20 13:55:49 +0100

    Script zum Synchronisieren unter Linux:
    Das Script synchronisiert alle Macros, Scripte und Plugins mit dem lokal installierten Fiji.

---
+++ syncWithFiji.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# file: syncWithFiji.sh
+# version: 20151120
+# info: Synchronisiert Macros, Scripte und Plugins mit Fiji.
+
+WD=$(dirname $0)
+FIJI=$HOME/Software/Fiji.app
+MACROS=$FIJI/macros/Eigene
+PLUGINS=$FIJI/plugins/Eigene
+
+echo -e "\nLösche archivierte Dateien aus Fiji..."
+for file in $WD/Archiv/*
+do
+	filePath=$MACROS/$(basename $file)
+	if [ -f $filePath ]
+	then
+		rm -v $filePath
+	fi
+	filePath=$PLUGINS/$(basename $file)
+	if [ -f $filePath ]
+	then
+		rm -v $filePath
+	fi
+done
+
+# Möchte man mit rsync nur bestimmte Dateien kopieren und Unterordner einschließen, dann wird es etwas komplizierter:
+# mit --include wählt man Dateien nach einem Pattern aus. Leider werden auch alle weiteren Dateien ausgewählt, wenn man sie nicht explizit ausschließt.
+# --exclude "*" schließt jedoch auch alle Verzeichnisse aus und eine Rekursion wird nicht durchgeführt.
+# Als alternative lässt sich --filter "-! */" verwenden, das alle Dateien ausschließt, außer die per --include gewählten.
+# Gefunden habe ich diesen Tipp auf [1].
+#
+# [1] http://blog.mudflatsoftware.com/blog/2012/10/31/tricks-with-rsync-filter-rules/
+echo -e "\nSynchronisiere Java-Plugins..."
+rsync -avz --include "*.jar" --include "*.class" --filter "-! */" --prune-empty-dirs $WD/java/ $PLUGINS/
+rsync -avz --include "*.jar" --include "*.class" --filter "-! */" --prune-empty-dirs $PLUGINS/ $WD/java/
+
+echo -e "\nSynchronisiere IJ-Macros..."
+rsync -avz  --include "*.ijm" --filter "-! */" --prune-empty-dirs $WD/ijm/ $MACROS/
+rsync -avz  --include "*.ijm" --filter "-! */" --prune-empty-dirs $MACROS/ $WD/ijm/
+
+echo -e "\nSynchronisiere JavaScript..."
+rsync -avz  --include "*.js" --filter "-! */" --prune-empty-dirs $WD/js/ $MACROS/
+rsync -avz  --include "*.js" --filter "-! */" --prune-empty-dirs $MACROS/ $WD/js/
+
+echo -e "\nSynchronisiere Jython..."
+rsync -avz --include "*.py" --filter "-! */" --prune-empty-dirs $WD/Jython/ $MACROS/
+rsync -avz --include "*.py" --filter "-! */" --prune-empty-dirs $MACROS/ $WD/Jython/
