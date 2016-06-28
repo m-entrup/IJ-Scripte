bc52311b7654bdf21f7eedab8804c3c783b5391b
Michael Entrup (b. Epping) <mail@m-entrup.de>
2016-06-28 15:17:00 +0200

    Correct_drift_(enhanced).py optimieren:
    Die bisherige Methode zur statistischen Auswertung der Fehlermatrix war fehlerhaft.
    Die neue Version betrachtet die Verschiebung gegenüber dem jeweiligen Verschiebungszentrum.
    Es wird jeweils der Mittelwert dieser Verschiebungen als Endergebnis verwendet.
    
    Ich habe außerdem Tabs durch jeweils 4 Leerzeichen ersetzt.

--- Jython/EFTEMj/Correct_drift_(enhanced).py
+++ Jython/EFTEMj/Correct_drift_(enhanced).py
@@ -1,9 +1,9 @@
 """
-file:		Correct_drift_(enhanced).py
-author:		Michael Entrup b. Epping (michael.entrup@wwu.de)
-version:	20160624
-info:		A script that corrects the drift between any number of images.
-			The images are not changed. A stack is created that holds the corrected images.
+file:       Correct_drift_(enhanced).py
+author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
+version:    20160624
+info:       A script that corrects the drift between any number of images.
+            The images are not changed. A stack is created that holds the corrected images.
 """
 
 from __future__ import with_statement, division
@@ -17,69 +17,78 @@ path.append(getProperty('fiji.dir') + '/plugins/Scripts/Plugins/EFTEMj/')
 import CrossCorrelation as CC
 import HelperDialogs as dialogs
 
-import operator, copy
+import operator, copy, pprint
 from ij import IJ, WindowManager
 
 def get_drift(i, j, images):
-	cc_img = CC.perform_correlation(images[i], images[j])
-	offset = CC.get_shift(cc_img)
-	# Mark the image as unchanged to close it without query
-	cc_img.changes = False
-	cc_img.close()
-	""" DEBUG
-	print 'Reference: %s at index %i' % (images[i],i)
-	print 'Drifted image: %s at index %i' % (images[j],j)
-	print 'Offset: %d,%d\n' % offset
-	"""
-	return offset
+    cc_img = CC.perform_correlation(images[i], images[j])
+    offset = CC.get_shift(cc_img)
+    # Mark the image as unchanged to close it without query
+    cc_img.changes = False
+    cc_img.close()
+    """ DEBUG
+    print 'Reference: %s at index %i' % (images[i],i)
+    print 'Drifted image: %s at index %i' % (images[j],j)
+    print 'Offset: %d,%d\n' % offset
+    """
+    return offset
 
-def mean_of_list_of_pairs(list):
-	x, y = zip(*list)
-	return (sum(x)/len(x), sum(y)/len(y))
+def perform_func_on_list_of_pairs(func, pair_list):
+    xs, ys = zip(*pair_list)
+    return (func(xs), func(ys))
+
+def mean_of_list_of_pairs(pair_list):
+    def func(vals):
+        return sum(vals)/len(vals)
+    return perform_func_on_list_of_pairs(func, pair_list)
+
+def center_of_list_of_pairs(pair_list):
+    def func(vals):
+        return (max(vals) + min(vals)) / 2
+    return perform_func_on_list_of_pairs(func, pair_list)
+
 
 def main():
-	img_count = IJ.getNumber("How many images do you want to correct?", 3)
-	# If canceld IJ.getNumber() returns -Integer.MAX_VALUE
-	if img_count < 3:
-		return False
-	image_ids = WindowManager.getIDList()
-	image_titles = [WindowManager.getImage(id).getTitle() for id in image_ids]
-	images_selected = dialogs.create_selection_dialog(image_titles, range(img_count), 'Select images for drift correction')
-	# dialogs.create_selection_dialog() returns None if canceled
-	if not images_selected:
-		return False
-	images = [WindowManager.getImage(image_ids[selection]) for selection in images_selected]
-	drift_matrix = [[]] * len(images)
-	for i in range(len(images)):
-		drift_matrix[i] = [[]] * len(images)
-		# print  'i=%i: ' % i, range(i + 1, len(images))
-		for j in range(i + 1, len(images)):
-			shift = get_drift(i, j, images)
-			""" DEBUG
-			print 'Appending to %i/%i:' % (i, j)
-			print shift
-			"""
-			drift_matrix[i][j] = [shift]
-	print drift_matrix
-	i_start = len(images) - 3
-	while i_start >= 0:
-		drift_matrix_updated = copy.deepcopy(drift_matrix)
-		for i in range(i_start, -1, -1):
-			for j in range(len(images)-1, 1, -1):
-				if j > (i+1):
-					""" INFO
-					Her is an example for the combination of zip() and asterisk:
-					list_of_pairs = [(1,4), (2,5), (3,6)]
-					*list_of_pairs -> (1,4), (2,5), (3,6)
-					zip(*zipped_list) -> [(1, 2, 3), (4, 5, 6)]
-					"""
-					x1, y1 = zip(*drift_matrix[i+1][j])
-					x2, y2 = zip(*drift_matrix[i][j-1])
-					combined = (sum(x1)/len(x1) + sum(x2)/len(x2), sum(y1)/len(y1) + sum(y2)/len(y2))
-					drift_matrix_updated[i][j].append(combined)
-		drift_matrix = drift_matrix_updated
-		print drift_matrix
-		i_start -= 1
-	print 'Final drif vector: ', map(mean_of_list_of_pairs, drift_matrix[0][1:])
-	
+    img_count = IJ.getNumber("How many images do you want to correct?", 3)
+    # If canceld IJ.getNumber() returns -Integer.MAX_VALUE
+    if img_count < 3:
+        return False
+    image_ids = WindowManager.getIDList()
+    image_titles = [WindowManager.getImage(id).getTitle() for id in image_ids]
+    images_selected = dialogs.create_selection_dialog(image_titles, range(img_count), 'Select images for drift correction')
+    # dialogs.create_selection_dialog() returns None if canceled
+    if not images_selected:
+        return False
+    images = [WindowManager.getImage(image_ids[selection]) for selection in images_selected]
+    drift_matrix = [[]] * len(images)
+    for i, _ in enumerate(drift_matrix):
+        drift_matrix[i] = [(0,0)] * len(images)
+    print 'Initial matrix: ', drift_matrix
+    for i in range(len(images)):
+        # print  'i=%i: ' % i, range(i + 1, len(images))
+        for j in range(i + 1, len(images)):
+            shift = get_drift(i, j, images)
+            """ DEBUG
+            print 'Appending to %i/%i:' % (i, j)
+            print shift
+            """
+            print 'Matrix element: ', i, j
+            print 'Adding value: ', shift
+            drift_matrix[i][j] = shift
+            drift_matrix[j][i] = tuple([-val for val in shift])
+    print 'Drift matrix:'
+    pprint.pprint(drift_matrix)
+    centers = [center_of_list_of_pairs(row) for row in drift_matrix]
+    print 'List of centers: ', centers
+    mod_matrix = [[tuple(map(operator.sub, cell, centers[i])) for cell in row]
+        for i, row in enumerate(drift_matrix)]
+    print 'Modified drift matrix:'
+    pprint.pprint(mod_matrix)
+    rot_matrix = [list(x) for x in zip(*mod_matrix)]
+    print 'Rotated matrix:'
+    pprint.pprint(rot_matrix)
+    shift_vector = [tuple(map(operator.neg, tup))
+        for tup in [mean_of_list_of_pairs(row) for row in rot_matrix]]
+    print 'Optimized shift vector: ', shift_vector
+
 main()
--- Jython/EFTEMj/CrossCorrelation.py
+++ Jython/EFTEMj/CrossCorrelation.py
@@ -1,10 +1,10 @@
 ﻿# coding=utf-8
 """
-file:		CrossCorrelation.py
-author:		Michael Entrup b. Epping (michael.entrup@wwu.de)
-version:	20160624
-info:		This module calculates the normalised Cross-correlation of two images.
-			There are aditional functions to style the result or find the position of the maximum.
+file:       CrossCorrelation.py
+author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
+version:    20160624
+info:       This module calculates the normalised Cross-correlation of two images.
+            There are aditional functions to style the result or find the position of the maximum.
 """
 
 from __future__ import with_statement, division
@@ -17,125 +17,124 @@ from ij.plugin import FFTMath
 from java.lang import Integer
 
 def perform_correlation(img1, img2):
-	"""
-	Return an ImagePlus that represents the normalized CrossCorrelation of two images.
-	:param img1: The ImagePlus to be used as reference.
-	:param img2: The ImagePlus its correlation to the first ImagePlus is calculated.
-	"""
-	norm = 1
-	for img in (img1, img2):
-		copy = img.duplicate()
-		img.show()
-		IJ.run(copy, "Square", "");
-		stat = copy.getStatistics(Stats.MEAN)
-		norm *= math.sqrt(stat.umean) * math.sqrt(img.getWidth()) * math.sqrt(img.getHeight())
-	# Include image names in square brackets to handle file names with spaces.
-	suffix = 1
-	prefix = 'Result'
-	title = prefix
-	while WindowManager.getImage(title):
-		title = prefix + '-' + str(suffix)
-		suffix += 1
-	IJ.run(img1, "FD Math...", "image1=[" + img1.getTitle() + "] operation=Correlate image2=[" + img2.getTitle() + "] result=[" + title + "]  do");
-	"""
-	Alternative to IJ.run where no configuration is possible:
-	cc = FFTMath()
-	cc.doMath(img1, img2)
-	"""
-	result = WindowManager.getImage(title)
-	IJ.run(result, "Divide...", "value=" + str(norm))
-	IJ.run(result, "Enhance Contrast", "saturated=0.0")
-	return result
+    """
+    Return an ImagePlus that represents the normalized CrossCorrelation of two images.
+    :param img1: The ImagePlus to be used as reference.
+    :param img2: The ImagePlus its correlation to the first ImagePlus is calculated.
+    """
+    norm = 1
+    for img in (img1, img2):
+        copy = img.duplicate()
+        IJ.run(copy, "Square", "")
+        stat = copy.getStatistics(Stats.MEAN)
+        norm *= math.sqrt(stat.umean) * math.sqrt(img.getWidth()) * math.sqrt(img.getHeight())
+    # Include image names in square brackets to handle file names with spaces.
+    suffix = 1
+    prefix = 'Result'
+    title = prefix
+    while WindowManager.getImage(title):
+        title = prefix + '-' + str(suffix)
+        suffix += 1
+    IJ.run(img1, "FD Math...", "image1=[" + img2.getTitle() + "] operation=Correlate image2=[" + img1.getTitle() + "] result=[" + title + "]  do");
+    """
+    Alternative to IJ.run where no configuration is possible:
+    cc = FFTMath()
+    cc.doMath(img1, img2)
+    """
+    result = WindowManager.getImage(title)
+    IJ.run(result, "Divide...", "value=" + str(norm))
+    IJ.run(result, "Enhance Contrast", "saturated=0.0")
+    return result
 
 
 def style_cc(cc_img):
-	"""
-	Styles an ImagePlus that shows a CrossCorrelation.
-	The maximum is marked by a point selection.
-	Scale bar and intensity calibration are added.
-	:param cc_img: An ImagePlus that shows a CrossCorrelation.
-	"""
-	new = ""
-	stat = cc_img.getStatistics(Stats.MIN_MAX)
-	min = round(50 * stat.min) / 50
-	max = round(50 * stat.max) / 50
-	cc_img.getProcessor().setMinAndMax(min, max)
-	if cc_img.getWidth() < 512:
-		scale = 2
-		while cc_img.getWidth() * scale < 512:
-			scale *= 2
-		title = cc_img.getTitle()
-		new = IJ.run(cc_img, "Scale...", "x=" + scale + " y=" + scale + " z=1.0 interpolation=None create");
-		cc_img.close()
-		new.rename(title)
-		cc_img = new
-	width = cc_img.getWidth()
-	height = cc_img.getHeight()
-	IJ.run(cc_img, "Remove Overlay", "");
-	cc_img.setRoi(Line(0.5 * width, 0, 0.5 * width, height))
-	IJ.run(cc_img, "Add Selection...", "");
-	cc_img.setRoi(Line(0, 0.5 * height, width, 0.5 * height))
-	IJ.run(cc_img, "Add Selection...", "")
-	IJ.run(cc_img, "Find Maxima...", "noise=" + str(width / 4) + " output=[Point Selection]")
-	IJ.run(cc_img, "Add Selection...", "");
-	IJ.run(cc_img, "Select None", "");
-	__create_scalebar(cc_img)
-	__create_calbar(cc_img)
-	if not cc_img.isVisible():
-		cc_img.show()
+    """
+    Styles an ImagePlus that shows a CrossCorrelation.
+    The maximum is marked by a point selection.
+    Scale bar and intensity calibration are added.
+    :param cc_img: An ImagePlus that shows a CrossCorrelation.
+    """
+    new = ""
+    stat = cc_img.getStatistics(Stats.MIN_MAX)
+    min = round(50 * stat.min) / 50
+    max = round(50 * stat.max) / 50
+    cc_img.getProcessor().setMinAndMax(min, max)
+    if cc_img.getWidth() < 512:
+        scale = 2
+        while cc_img.getWidth() * scale < 512:
+            scale *= 2
+        title = cc_img.getTitle()
+        new = IJ.run(cc_img, "Scale...", "x=" + scale + " y=" + scale + " z=1.0 interpolation=None create");
+        cc_img.close()
+        new.rename(title)
+        cc_img = new
+    width = cc_img.getWidth()
+    height = cc_img.getHeight()
+    IJ.run(cc_img, "Remove Overlay", "");
+    cc_img.setRoi(Line(0.5 * width, 0, 0.5 * width, height))
+    IJ.run(cc_img, "Add Selection...", "");
+    cc_img.setRoi(Line(0, 0.5 * height, width, 0.5 * height))
+    IJ.run(cc_img, "Add Selection...", "")
+    IJ.run(cc_img, "Find Maxima...", "noise=" + str(width / 4) + " output=[Point Selection]")
+    IJ.run(cc_img, "Add Selection...", "")
+    IJ.run(cc_img, "Select None", "")
+    __create_scalebar(cc_img)
+    __create_calbar(cc_img)
+    if not cc_img.isVisible():
+        cc_img.show()
 
 def get_max(cc_img):
-	"""
-	Finds the maximum of an image and returns it as a list of the length 2.
-	This function is designed for use on CrossCorrelation images.
-	:param cc_img: An ImagePlus showing a CrossCorrelation.
-	"""
-	width = cc_img.getWidth()
-	IJ.run(cc_img, "Find Maxima...", "noise=" + str(width / 4) + " output=[Point Selection]")
-	roi = cc_img.getRoi()
-	if roi.getClass() == PointRoi:
-		return (roi.getBounds().x, roi.getBounds().y)
-	else:
-		return (None, None)
+    """
+    Finds the maximum of an image and returns it as a list of the length 2.
+    This function is designed for use on CrossCorrelation images.
+    :param cc_img: An ImagePlus showing a CrossCorrelation.
+    """
+    width = cc_img.getWidth()
+    IJ.run(cc_img, "Find Maxima...", "noise=" + str(width / 4) + " output=[Point Selection]")
+    roi = cc_img.getRoi()
+    if roi.getClass() == PointRoi:
+        return (roi.getBounds().x, roi.getBounds().y)
+    else:
+        return (None, None)
 
 def get_shift(cc_img):
-	"""
-	Finds the maximum of an image and returns the offset to the centre.
-	This function is designed for use on CrossCorrelation images.
-	:param cc_img: An ImagePlus showing a CrossCorrelation.
-	"""	
-	x, y = get_max(cc_img)
-	x_off = x - cc_img.getWidth() / 2
-	y_off = y - cc_img.getHeight() / 2
-	return x_off, y_off
+    """
+    Finds the maximum of an image and returns the offset to the centre.
+    This function is designed for use on CrossCorrelation images.
+    :param cc_img: An ImagePlus showing a CrossCorrelation.
+    """
+    x, y = get_max(cc_img)
+    x_off = x - cc_img.getWidth() / 2
+    y_off = y - cc_img.getHeight() / 2
+    return x_off, y_off
 
 def __create_scalebar(imp):
-	"""
-	Creates a scale bar and adds it to an ImagePlus. Nothing is returned.
-	:param imp: The imagePlus the scale bar is added to.
-	"""
-	width = imp.getWidth()
-	fontSize = width / 4096 * 150
-	scaleBarColor = "White"
-	cal = imp.getCalibration()
-	pixelWidth = cal.getX(1.)
-	if width*pixelWidth > 10:
-		barWidth = 10 * round(width*pixelWidth / 80)
-	elif (width*pixelWidth > 1):
-		barWidth = math.floor((width*pixelWidth / 8) + 1)
-	else:
-		barWidth = 0.01 * math.floor(100 * width*pixelWidth / 8)
+    """
+    Creates a scale bar and adds it to an ImagePlus. Nothing is returned.
+    :param imp: The imagePlus the scale bar is added to.
+    """
+    width = imp.getWidth()
+    fontSize = width / 4096 * 150
+    scaleBarColor = "White"
+    cal = imp.getCalibration()
+    pixelWidth = cal.getX(1.)
+    if width*pixelWidth > 10:
+        barWidth = 10 * round(width*pixelWidth / 80)
+    elif (width*pixelWidth > 1):
+        barWidth = math.floor((width*pixelWidth / 8) + 1)
+    else:
+        barWidth = 0.01 * math.floor(100 * width*pixelWidth / 8)
 
-	barHeight = fontSize / 3
-	#print(barWidth, barHeight, fontSize, scaleBarColor)
-	IJ.run(imp, "Scale Bar...", "width=%d height=%d font=%d color=%s background=None location=[Lower Right] bold overlay" % (barWidth, barHeight, fontSize, scaleBarColor))
+    barHeight = fontSize / 3
+    #print(barWidth, barHeight, fontSize, scaleBarColor)
+    IJ.run(imp, "Scale Bar...", "width=%d height=%d font=%d color=%s background=None location=[Lower Right] bold overlay" % (barWidth, barHeight, fontSize, scaleBarColor))
 
 
 def __create_calbar(imp):
-	"""
-	Creates a calibration bar and adds it to an ImagePlus. Nothing is returned.
-	:param imp: The imagePlus the calibration bar is added to.
-	"""
-	fontSize = 10;
-	zoom = imp.getWidth() / 4096 * 10;
-	IJ.run(imp, "Calibration Bar...", "location=[Upper Right] fill=White label=Black number=3 decimal=2 font=%d zoom=%d overlay" % (fontSize, zoom));
+    """
+    Creates a calibration bar and adds it to an ImagePlus. Nothing is returned.
+    :param imp: The imagePlus the calibration bar is added to.
+    """
+    fontSize = 10;
+    zoom = imp.getWidth() / 4096 * 10;
+    IJ.run(imp, "Calibration Bar...", "location=[Upper Right] fill=White label=Black number=3 decimal=2 font=%d zoom=%d overlay" % (fontSize, zoom));
