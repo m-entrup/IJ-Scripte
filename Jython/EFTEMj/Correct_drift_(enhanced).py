454b90feff0e22a54d394a98e31039eb33d6c78f
Michael Entrup (b. Epping) <mail@m-entrup.de>
2016-06-28 16:36:04 +0200

    Correct_drift_(enhanced).py fertigstellen: Das Script enthält nun die fehlende Translation der Bilder. Die korrigierten Bilder werden als neuer Stack ausgegeben. Die angewendete Verschiebung ist im Slice-Label enthalten.

--- Jython/EFTEMj/Correct_drift_(enhanced).py
+++ Jython/EFTEMj/Correct_drift_(enhanced).py
@@ -18,7 +18,8 @@ import CrossCorrelation as CC
 import HelperDialogs as dialogs
 
 import operator, copy, pprint
-from ij import IJ, WindowManager
+from ij import IJ, WindowManager, ImageStack, ImagePlus
+from ij.plugin import Duplicator
 
 def get_drift(i, j, images):
     cc_img = CC.perform_correlation(images[i], images[j])
@@ -44,6 +45,21 @@ def center_of_list_of_pairs(pair_list):
         return (max(vals) + min(vals)) / 2
     return perform_func_on_list_of_pairs(func, pair_list)
 
+def shift_images(img_list, shift_vector):
+    """
+    returns a list of new images that are shifted by the given values.
+    :param img_list: A list of images to be shifted.
+    :param shift_vector: A List of x-, y-coordinates to define the shift per image.
+    """
+    shifted_list = [Duplicator().run(img) for img in img_list]
+    def make_title(i):
+        old_title = img_list[i].getTitle()
+        new_title = 'DK$%f$%f$%s' % (shift_vector[i][0],shift_vector[i][1], old_title)
+        return new_title
+    [img.setTitle(make_title(i)) for i, img in enumerate(shifted_list)]
+    [IJ.run(img, 'Translate...', 'x=%d y=%d interpolation=None' % (shift_vector[i][0], shift_vector[i][1]))
+        for i, img in enumerate(shifted_list)]
+    return shifted_list
 
 def main():
     img_count = IJ.getNumber("How many images do you want to correct?", 3)
@@ -60,7 +76,7 @@ def main():
     drift_matrix = [[]] * len(images)
     for i, _ in enumerate(drift_matrix):
         drift_matrix[i] = [(0,0)] * len(images)
-    print 'Initial matrix: ', drift_matrix
+    # print 'Initial matrix: ', drift_matrix
     for i in range(len(images)):
         # print  'i=%i: ' % i, range(i + 1, len(images))
         for j in range(i + 1, len(images)):
@@ -69,23 +85,27 @@ def main():
             print 'Appending to %i/%i:' % (i, j)
             print shift
             """
-            print 'Matrix element: ', i, j
-            print 'Adding value: ', drift
+            # print 'Matrix element: ', i, j
+            # print 'Adding value: ', drift
             drift_matrix[i][j] = drift
             drift_matrix[j][i] = tuple([-val for val in drift])
-    print 'Drift matrix:'
-    pprint.pprint(drift_matrix)
+    # print 'Drift matrix:'
+    # pprint.pprint(drift_matrix)
     centers = [center_of_list_of_pairs(row) for row in drift_matrix]
-    print 'List of centers: ', centers
+    # print 'List of centers: ', centers
     mod_matrix = [[tuple(map(operator.sub, cell, centers[i])) for cell in row]
         for i, row in enumerate(drift_matrix)]
-    print 'Modified drift matrix:'
-    pprint.pprint(mod_matrix)
+    # print 'Modified drift matrix:'
+    # pprint.pprint(mod_matrix)
     rot_matrix = [list(x) for x in zip(*mod_matrix)]
-    print 'Rotated matrix:'
-    pprint.pprint(rot_matrix)
+    # print 'Rotated matrix:'
+    # pprint.pprint(rot_matrix)
     shift_vector = [tuple(map(operator.neg, tup))
         for tup in [mean_of_list_of_pairs(row) for row in rot_matrix]]
-    print 'Optimized shift vector: ', shift_vector
+    # print 'Optimized shift vector: ', shift_vector
+    stack = ImageStack(images[0].getWidth(), images[0].getHeight())
+    [stack.addSlice(img.getTitle(), img.getProcessor()) for img in shift_images(images, shift_vector)]
+    corrected_stack = ImagePlus('Drift corrected stack', stack)
+    corrected_stack.show()
 
 main()
