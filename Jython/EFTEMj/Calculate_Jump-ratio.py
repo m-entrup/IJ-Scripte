59aec2682781aaed0a737a54ae482fc5fca67c29
Michael Entrup <mail@m-entrup.de>
2016-07-06 17:53:34 +0200

    Jump-ratio.py verbessert:
    Bei der Driftkorrektur besteht nun die Wahl zwischen CC und SIFT.
    In der funktion get_setup() wird ein GenericDialog erzeugt, um Parameter
    abzufragen.

--- Jython/EFTEMj/Calculate_Jump-ratio.py
+++ Jython/EFTEMj/Calculate_Jump-ratio.py
@@ -1,11 +1,11 @@
-"""
+'''
 file:       Calculate_Jump-ratio.py
 author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
-version:    20160616
+version:    20160706
 info:       A script that calculates the Jump-Ratio of two images.
             The second image is devided by the first one.
             A drift correction is performed. The first image is shiftet towards to the second one.
-"""
+'''
 
 from sys import modules, path
 # When using own modules it is necessary to use 'sys.modules.clear()'.
@@ -16,25 +16,47 @@ from java.lang.System import getProperty
 path.append(getProperty('fiji.dir') + '/plugins/Scripts/Plugins/EFTEMj/')
 import CorrectDrift as drift
 import HelperDialogs as dialogs
+import Tools as tools
 
-from ij import IJ
+from ij import IJ, WindowManager
+from ij.gui import GenericDialog
 from ij.plugin import ImageCalculator
 from ij.process import ImageStatistics as Stats
 
+
+def get_setup():
+    ''' Returns the drift correction mode and two image.'''
+    options = ['Scale-invariant feature transform', 'Normalized cross-correlation']
+    modes = ['SIFT', 'CC']
+    gd = GenericDialog('Jump-ratio setup')
+    gd.addMessage('Select the mode  for drift correction\n' +
+        'and the images to process.')
+    gd.addChoice('Mode:', options, options[0])
+    image_ids = WindowManager.getIDList()
+    image_titles = [WindowManager.getImage(id).getTitle() for id in image_ids]
+    gd.addMessage('Post-edge is divided by the pre-edge.')
+    gd.addChoice('Pre-edge', image_titles, image_titles[0])
+    gd.addChoice('Post-edge', image_titles, image_titles[1])
+    gd.showDialog()
+    if gd.wasCanceled():
+        return [None]*3
+    mode = modes[gd.getNextChoiceIndex()]
+    img1 = WindowManager.getImage(image_ids[gd.getNextChoiceIndex()])
+    img2 = WindowManager.getImage(image_ids[gd.getNextChoiceIndex()])
+    return mode, img1, img2
+
+
 def run_script():
-    img_titles = dialogs.get_image_titles()
-    if len(img_titles) < 2:
+    selected_mode, img1, img2 = get_setup()
+    if not selected_mode:
         return
-    selected_ids = dialogs.create_selection_dialog(img_titles, range(2))
-    if not selected_ids:
-        return
-    img1, img2 = dialogs.get_images(selected_ids)
-    img2, img1 = drift.correct_drift(img2, img1, False)
-    img_ratio = ImageCalculator().run("Divide create", img2, img1)
+    corrected_stack = drift.get_corrected_stack((img1, img2), mode = selected_mode)
+    img1, img2 = tools.stack_to_list_of_imp(corrected_stack)
+    img_ratio = ImageCalculator().run('Divide create', img2, img1)
     img_ratio.setTitle('Jump-ratio [%s divided by %s]' % (img2.getShortTitle(), img1.getShortTitle()))
     img_ratio.changes = True
     img_ratio.show()
-    IJ.run(img_ratio, "Enhance Contrast", "saturated=0.35")
+    IJ.run(img_ratio, 'Enhance Contrast', 'saturated=0.35')
     # We want to optimise the lower displaylimit:
     minimum = img_ratio.getProcessor().getMin()
     maximum = img_ratio.getProcessor().getMax()
@@ -47,6 +69,7 @@ def run_script():
         else:
             img_ratio.getProcessor().setMinAndMax(0, maximum)
         img_ratio.updateAndDraw()
+
 
 if __name__ == '__main__':
     run_script()
