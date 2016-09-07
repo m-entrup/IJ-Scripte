8dcde2ce7d50f72f248d46650f6ad6308d8b9f14
Michael Entrup (b. Epping) <mail@m-entrup.de>
2016-09-07 15:14:53 +0200

    2tes Beispiel Script fir das ImageJ-Wiki fertigstellen:
    Das Script enthält nun weitere Funktionen.
    Alle Besonderheiten sind ausführlich dokumentiert.

--- Jython/Jython Scripting (wiki)/ScriptParameters.py
+++ Jython/Jython Scripting (wiki)/ScriptParameters.py
@@ -1,44 +1,22 @@
-# @Short(label='Image size', value=512) img_size
-# @Double(label='Image amplitude', value=1.0) amplitude
-# @Short(label='Spacing', value=16) spacing
+# @String(value='Please set some parameters.', visibility='MESSAGE') message
+# @Short(label='Image size', value=512, min=128, max=2048, stepSize=128, style="slider") img_size
+# @Double(label='Image amplitude', value=100) amplitude
+# @Short(label='Spacing', value=16, min=8) spacing
 
 # The parameters in front of this comment are populated before the script runs.
+# Details on Script parameters can be found at
+# http://imagej.net/Script_parameters
 
 # The module __future__ contains some useful functions:
 # https://docs.python.org/2/library/__future__.html
 from __future__ import with_statement, division
-import math
-from ij import IJ
-
-def split_list(alist, wanted_parts=1):
-	"""Split a list to the given number of parts."""
-	length = len(alist)
-	return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
-			 for i in range(wanted_parts) ]
-
-def fft_filter(imp):
-	"""Removing noise from an image by using a FFT filter"""
-	IJ.run(imp, "FFT", "")
-	from ij import WindowManager as wm
-	fft = wm.getImage("FFT of " + imp.getTitle())
-	IJ.run(fft, "Find Maxima...", "noise=64 output=[Point Selection] exclude")
-	# Enlarging the point selectins from Find Maxima.
-	IJ.run(fft, "Enlarge...", "enlarge=2")
-	# Inverting the selection.
-	IJ.run(fft, "Make Inverse", "")
-	IJ.run(fft, "Macro...", "code=v=0");
-	IJ.run(fft, "Inverse FFT", "")
-	fft.changes = False
-	fft.close()
-	imp_filtered = wm.getImage("Inverse FFT of " + imp.getTitle())
-	imp_filtered.setTitle("Filtered " + imp.getTitle())
-	imp_filtered.changes = False
 
 # It's best practice to create a function that contains the code that is executed when running the script.
 # This enables us to stop the script by just calling return.
 def run_script():
 	# We can use import inside of code blocks to limit the scope.
-	from ij import ImagePlus
+	import math
+	from ij import IJ, ImagePlus
 	from ij.process import FloatProcessor
 	blank = IJ.createImage("Blank", "32-bit black", img_size, img_size, 1)
 	# This create a list of lists. Each inner list represents a line.
@@ -57,12 +35,85 @@ def run_script():
 			pixel_matrix[x][y] = amplitude * val
 	# The constructor of FloatProcessor works fine with a 2D Python list.
 	crystal = ImagePlus("Crystal", FloatProcessor(pixel_matrix))
+	# Crop without selection is used to duplicate an image.
 	crystal_with_noise = crystal.crop()
 	crystal_with_noise.setTitle("Crystal with noise")
 	IJ.run(crystal_with_noise, "Add Specified Noise...", "standard=%d" % int(amplitude/math.sqrt(2)))
+	# As this is a demo, we don't want to be ask to save an image on closing it.
+	# In Python True and False start with capital letters.
+	crystal_with_noise.changes = False
 	crystal.show()
 	crystal_with_noise.show()
-	fft_filter(crystal_with_noise)
+	filtered = fft_filter(crystal_with_noise)
+	# We create a lambda function to be used as a parameter of img_calc().
+	subtract = lambda values: values[0] - values[1]
+	""" This is a short form for:
+	def subtract(values):
+		return values[0] - values[1]
+	"""
+	difference = img_calc(subtract, crystal, filtered, title="Difference")
+	difference.show()
+	minimum = img_calc(min, crystal, filtered, title="Minimum")
+	minimum.show()
+	for imp in (crystal, crystal_with_noise, filtered, difference, minimum):
+		IJ.run(imp, "Measure", "")
+
+# Functions can be defined after they are used.
+# This is only possible if the main code is encapsulated into a function.
+# The main function has to be called at the end of the script.
+
+def img_calc(func, *imps, **kwargs):
+	"""Runs the given function on each pixel of the given list of images.
+	An additional parameter, the title of the result, is passed as keyword parameter.
+	We assume that each image has the same size. This is not checked by this function.
+	"""
+	# If the keyword parameter is not passed, it is set to a default value.
+	if not kwargs['title']:
+		kwargs['title'] = "Result"
+	# This is a 2D list: list[number of images][pixels per image] .
+	pixels = [imp.getProcessor().getPixels() for imp in imps]
+	# The function is called pixel by pixel.
+	# zip(*pixels) rotates the 2D list: list[pixels per image][mumber of images].
+	result = [func(vals) for vals in zip(*pixels)]
+	# result is a 1D list and can be used to create an ImagePlus object.
+	from ij import ImagePlus
+	from ij.process import FloatProcessor
+	return ImagePlus(kwargs['title'], FloatProcessor(img_size, img_size, result))
+
+def split_list(alist, wanted_parts=1):
+	"""Split a list to the given number of parts."""
+	length = len(alist)
+	# alist[a:b:step] is used to get only a subsection of the list 'alist'.
+	# alist[a:b] is the same as [a:b:1].
+	# '//' is an integer division.
+	# Without 'from __future__ import division' '/' would be an integer division. 
+	return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+			 for i in range(wanted_parts) ]
+
+def fft_filter(imp):
+	""" Removing noise from an image by using a FFT filter
+	This are operations copied from the ImageJ macro recorder.
+	Jython does not complain when you forget to remove the semicolons.
+	"""
+	from ij import IJ
+	IJ.run(imp, "FFT", "");
+	# No ImagePlus is returned by the FFT function of ImageJ.
+	# We need to use the WindowManager to select the newly created image.
+	from ij import WindowManager as wm
+	fft = wm.getImage("FFT of " + imp.getTitle())
+	IJ.run(fft, "Find Maxima...", "noise=64 output=[Point Selection] exclude");
+	# Enlarging the point selectins from Find Maxima.
+	IJ.run(fft, "Enlarge...", "enlarge=2");
+	# Inverting the selection.
+	IJ.run(fft, "Make Inverse", "");
+	IJ.run(fft, "Macro...", "code=v=0");
+	IJ.run(fft, "Inverse FFT", "");
+	fft.changes = False
+	fft.close()
+	imp_filtered = wm.getImage("Inverse FFT of " + imp.getTitle())
+	imp_filtered.setTitle("Filtered " + imp.getTitle())
+	imp_filtered.changes = False
+	return imp_filtered
 
 
 # If a Jython script is run, the variable __name__ contains the string '__main__'.
