5d06c1f257fb716c219b58073631bf722f6a5614
Michael Entrup (b. Epping) <mail@m-entrup.de>
2017-02-02 15:44:55 +0100

    measureNIC.py auf geringeren Speicherverbrauch optimieren:
    die alte Version erzeugt zu viele neue Objekte, die den Speicher füllen.
    Das verarbeiten eines 800 MB Stacks verbraucht mehr als 24 GB Arbeitsspeicher.
    
    Die neue Version referenziert auf das originale Bild und erzeugt keinen neuen Datensatz.
    Bei kleinen Stacks läuft das Script dadurch minimal langsamer.
    Jedoch ist es auf diese Weise erst möglich große Stacks zu verarbeiten.

--- Jython/measureNIC.py
+++ Jython/measureNIC.py
@@ -20,70 +20,55 @@ from org.apache.commons.math3.fitting import SimpleCurveFitter
 from org.apache.commons.math3.fitting import WeightedObservedPoints
 from org.apache.commons.math3.analysis import ParametricUnivariateFunction
 
-class PixelObject:
+pattern_eloss = re.compile('(\d+(?:\.\d+)?)eV')
 
-    def __init__(self, x, y, vals_x, vals_y):
-        self.x = x
-        self.y = y
-        self.vals_x = vals_x
-        self.vals_y = vals_y
-        self.points = WeightedObservedPoints()
-        for x, y in zip(vals_x, vals_y):
-            self.points.add(1, x, y)
-        self.offset = 0
-        self.width = 0
-        self.amplitude = 0
+class ZProfiles:
 
-class ImageArray(list):
+    def __init__(self, imp, offset):
+        self.imp = imp
+        self.loss_offset = offset
+        self.w = imp.getWidth()
+        self.h = imp.getHeight()
+        self.index = self.w * self.h
+        self.offset = [0] * (self.w * self.h)
+        self.width = [0] * (self.w * self.h)
+        self.amplitude = [0] * (self.w * self.h)
+        self.elosses = []
+        self.pixels = []
+        for z in range(self.imp.getStackSize()):
+            label = self.imp.getStack().getShortSliceLabel(z+1)
+            match = pattern_eloss.search(label)
+            loss = float(match.group(1)) - self.loss_offset
+            self.elosses.append(loss)
+            self.pixels.append(self.imp.getStack().getPixels(z + 1))
+            IJ.showProgress(z + 1, self.imp.getStackSize())
 
-    def __init__(self, imp):
-        self.width = imp.getWidth()
-        self.height = imp.getHeight()
 
-def stack_to_array(imp):
-    array = ImageArray(imp)
-    elosses = []
-    stack = imp.getImageStack()
-    pattern = re.compile('(\d+(?:\.\d+)?)eV')
-
-    # We guess that the stack is sorted by energy loss and that the ZLP is at the centre of the stack.
-    offset_match = pattern.search(stack.getShortSliceLabel(int(math.floor(imp.getStackSize() / 2)) + 1))
-    offset = float(offset_match.group(1))
-    offset = IJ.getNumber("Enter energy loss offset: ", offset)
-    if (offset == IJ.CANCELED) :
-        return IJ.CANCELED
-
-    IJ.showStatus("Preparing the data...")
-    for z in range(imp.getStackSize()):
-        label = stack.getShortSliceLabel(z+1)
-        match = pattern.search(label)
-        elosses.append(float(match.group(1)) - offset)
-
-    '''
-    Create an object for each pixel of the stacks projection:
-    Each object represents an EEL spectrum of the ZLP.
-    '''
-    for y in range(imp.getHeight()):
-        for x in range(imp.getWidth()):
-            valsY = []
-            for z in range(imp.getStackSize()):
-                valsY.append(stack.getVoxel(x, y, z))
-            array.append(PixelObject(x, y, elosses, valsY))
-        IJ.showProgress(y+1, imp.getHeight())
-    return array
-
-def create_nic(array):
-    fp = FloatProcessor(array.width, array.height)
-    for obj in array:
-        fp.setf(obj.x, obj.y, obj.offset)
+def create_nic(data):
+    fp = FloatProcessor(data.w, data.h)
+    for y in range(data.h):
+        for x in range(data.w):
+            index = x + y * data.w
+            fp.setf(index, data.offset[index])
     imp = ImagePlus("NIC", fp)
     return imp
 
-def create_width(array):
-    fp =  FloatProcessor(array.width, array.height)
-    for obj in array:
-        fp.setf(obj.x, obj.y, obj.width)
+def create_width(data):
+    fp =  FloatProcessor(data.w, data.h)
+    for y in range(data.h):
+        for x in range(data.w):
+            index = x + y * data.w
+            fp.setf(index, data.width[index])
     imp = ImagePlus("width", fp)
+    return imp
+
+def create_amplitude(data):
+    fp =  FloatProcessor(data.w, data.h)
+    for y in range(data.h):
+        for x in range(data.w):
+            index = x + y * data.w
+            fp.setf(index, data.amplitude[index])
+    imp = ImagePlus("amplitude", fp)
     return imp
 
 class Zlp(ParametricUnivariateFunction):
@@ -105,20 +90,30 @@ def create_fitter():
     fitter = SimpleCurveFitter.create(func, params_start)
     return fitter
 
-def fit_gauss(fitter, obj):
-    obj.amplitude, obj.offset, obj.width = fitter.fit(obj.points.toList())
+def fit_gauss(fitter, data, index):
+    points = WeightedObservedPoints()
+    for i, z in enumerate(data.elosses):
+        points.add(1, z, data.pixels[i][index])
+    amplitude, offset, width = fitter.fit(points.toList())
+    data.offset[index] = offset
+    data.width[index] = width
+    data.amplitude[index] = amplitude
 
-def multithread_func(func, array):
+def multithread_func(func, data):
     threads = Array.newInstance(Thread, Runtime.getRuntime().availableProcessors())
-    print('Processing %d profiles with %d threads.' % (len(array), len(threads)))
+    print('Processing %d profiles with %d threads.' % (data.index, len(threads)))
     ai = AtomicInteger(0)
     progress = AtomicInteger(1)
     class Body(Runnable):
+
+        def __init__(self):
+            self.fitter = create_fitter()
+
         def run(self):
-            for i in (ai.getAndIncrement() for _ in  range(len(array))):
-                if i < len(array):
-                    func(array.fitter, array[i])
-                    IJ.showProgress(progress.getAndIncrement(), len(array))
+            for i in (ai.getAndIncrement() for _ in  range(data.index)):
+                if i < data.index:
+                    func(self.fitter, data, i)
+                    IJ.showProgress(progress.getAndIncrement(), data.index)
     for i in range(len(threads)):
         threads[i] = Thread(Body())
         threads[i].start()
@@ -135,21 +130,25 @@ def main():
         # Exit if IJ.getImage() was not able to return an image.
         return
 
-    binnedImp = Duplicator().run(inputImp)
-    bin = round(inputImp.getWidth() / 128)
-    bin = int(IJ.getNumber("Set the binning factor:", bin))
-    if bin == IJ.CANCELED:
+    # We guess that the stack is sorted by energy loss and that the ZLP is at the centre of the stack.
+    offset_match = pattern_eloss.search(
+        inputImp.getStack().getShortSliceLabel(
+            int(math.floor(inputImp.getStackSize() / 2)) + 1
+        )
+    )
+    offset = float(offset_match.group(1))
+    offset = IJ.getNumber("Enter energy loss offset: ", offset)
+    if (offset == IJ.CANCELED) :
         return
+
     IJ.showStatus("Preparing the data...")
     IJ.showProgress(0)
-    IJ.run(binnedImp, "Bin...", "x=%d y=%d bin=Average" % (bin, bin))
-    zProfiles = stack_to_array(binnedImp)
+    zProfiles = ZProfiles(inputImp, offset)
     if zProfiles == IJ.CANCELED:
         return
     IJ.showStatus("Calculating the NIC...")
     IJ.showProgress(0)
     start_time = time.time()
-    zProfiles.fitter = create_fitter()
     '''
     for profile in zProfiles:
         fit_gauss(zProfiles.fitter, profile)
@@ -158,6 +157,7 @@ def main():
     print('Fitting took %.3fs to finish.' % (time.time() - start_time,))
     create_nic(zProfiles).show()
     create_width(zProfiles).show()
+    create_amplitude(zProfiles).show()
 
 if __name__ == '__main__':
     main()
